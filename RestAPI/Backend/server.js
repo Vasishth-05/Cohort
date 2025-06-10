2a24b99c7b18fd5fdf8e66a2b80b1dba718eeff2
Vasishth <learnvasishth@gmail.com>
2025-06-10 14:30:12 +0530

    added folders

--- RestAPI/Backend/server.js
+++ RestAPI/Backend/server.js
@@ -1,1 +1,15 @@
-console.log("hare krishna");
+const express = require("express");
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', function(req,res){
+    res.json({
+        message: 'hare krsna'
+    })
+})
+
+app.listen(port,() => 
+    console.log(`Server is running at the port ${port}`)
+)
