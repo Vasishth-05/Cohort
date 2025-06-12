9eda20eb5ed0eda57cbdd1a93001bfa813d85c49
Vasishth <learnvasishth@gmail.com>
2025-06-12 20:29:21 +0530

    added mongoose connections

--- RestAPI/Backend/config/db.js
+++ RestAPI/Backend/config/db.js
@@ -1,1 +1,14 @@
 const moongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        const conn = await moongoose.connect(process.env.MONGO_URL);
+
+        console.log(`MongoDB connected : ${conn.connection.host}`.bgBlue.underline);
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
--- RestAPI/Backend/server.js
+++ RestAPI/Backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const colors = require('colors');
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require("./config/db");
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 app.use(express.json())
@@ -11,6 +15,7 @@ app.use (express.urlencoded({extended: false}))
 app.use('/api/goals', require("./Routes/goalRoutes"))
 
 app.use(errorHandler)
+app.use(connectDB)
 
 app.listen(port,() => 
     console.log(`Server is running at the port ${port}`)
